e201b5f83ed90782651a0597194eceed750d7b52
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-02-26 10:24:38 +1300

    Update tampermonkeycode.user.js

--- tampermonkeycode.user.js
+++ tampermonkeycode.user.js
@@ -29,7 +29,7 @@
         var ask = true;
         var originalTime = (new Date()).getTime();
         while(ask){
-            var str = prompt('Please enter your nation/puppet name. Follow it with the string ~/fin if you have inserted all your puppets/nations you will be using for N-Day. Enter the same string without any preceding text if you forgot to enter the string with your last puppet.').toLowerCase().replaceAll(' ', '_');
+            var str = prompt('Please enter your nation/puppet name. Follow it with the string ~/fin if you have inserted all your puppets/nations you will be using for N-Day. Enter the same string without any preceding text if you forgot to enter the string with your last puppet. To enter multiple nations, separate them with a comma character.').toLowerCase().replaceAll(' ', '_');
             var request = new XMLHttpRequest;
             if(str.indexOf('~/fin') == str.length - 5){
                 ask = false;
@@ -42,8 +42,10 @@
                 if(request.status == 404 || request.status == 400){
                     alert('No nation by this name exists.');
                 }else{
-                    puppets[puppets.length] = str.split('~/')[0];
-                    passwords[passwords.length] = prompt('Insert password for ' + str.split('~/')[0])
+                    var pass = ('Insert password/s')
+                    for(var ptem = 0; ptem < str.split('~/')[0].length; ptem++){
+                        puppets[puppets.length] = str.split('~/')[0].split(',')[ptem];
+                        passwords[passwords.length] = pass.split(',')[ptem];
                 }
             }
         }
